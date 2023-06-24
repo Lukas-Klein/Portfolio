4d7cb6b0935ff122ba0ac28b051013f1213abfdd
Lukas Klein <lukas.klein@telekom.de>
2023-06-24 13:58:44 +0200

    inital commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import DarkModeButton from './components/darkModeButton.svelte';
+  import app from "./app.css"
+  import HeaderTag from './components/headerTag.svelte';
+  import { P } from 'flowbite-svelte';
+</script>
+
+<nav class="navHeader">
+  <div class="menuL">  <HeaderTag title="lukas" />
+  </div>
+  <div class="menuR">
+  <HeaderTag title="projects" />
+  <HeaderTag title="career" />
+  <HeaderTag title="contact" />
+  <DarkModeButton />
+</div>
+<P>123123123</P>
+</nav>
+
---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+const config = {
+  content: [
+    "./src/**/*.{html,js,svelte,ts}",
+    "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
+  ],
+
+  plugins: [
+    require('flowbite/plugin')
+  ],
+
+  darkMode: 'class',
+
+  theme: {
+    extend: {
+      colors: {
+        // emerald
+        primary: {"50":"#ecfdf5","100":"#d1fae5","200":"#a7f3d0","300":"#6ee7b7","400":"#34d399","500":"#10b981","600":"#059669","700":"#047857","800":"#065f46","900":"#064e3b"}
+}
+    }
+  }
+};
+
+module.exports = config;
